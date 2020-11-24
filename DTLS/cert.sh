8f2396a28b39d4c1e524149e6ac4bbe35c41f6f0
Wonhee Han <hanwh@hunature.net>
2020-11-24 13:22:39 +0900

    fix: ERROR:There is already a certificate

--- DTLS/cert.sh
+++ DTLS/cert.sh
@@ -10,18 +10,18 @@ echo 01 > ca-db-serial
 
 # Certificate Authority
 $OPENSSL_BIN req -nodes -x509 -newkey rsa:2048 -days 365 -keyout ca-key.pem -out ca-cert.pem \
-	-subj "/C=KR/ST=Seoul/L=Uljiro/O=Hunature/OU=MakersAIOT/CN=hanwh"
+	-subj "/C=KR/ST=Seoul/L=Uljiro/O=Hunature/OU=MakersAIOT/CN=hanwh-ca"
 
 # Server Certificate
 $OPENSSL_BIN req -nodes -new -newkey rsa:2048 -keyout server-key.pem -out server.csr \
-	-subj "/C=KR/ST=Seoul/L=Uljiro/O=Hunature/OU=MakersAIOT/CN=hanwh"
+	-subj "/C=KR/ST=Seoul/L=Uljiro/O=Hunature/OU=MakersAIOT/CN=hanwh-server"
 
 # Sign Server Certificate
 $OPENSSL_BIN ca -config $SCRIPT_DIR/ca.conf -days 365 -in server.csr -out server-cert.pem
 
 # Client Certificate
 $OPENSSL_BIN req -nodes -new -newkey rsa:2048 -keyout client-key.pem -out client.csr \
-	-subj "/C=KR/ST=Seoul/L=Uljiro/O=Hunature/OU=MakersAIOT/CN=hanwh"
+	-subj "/C=KR/ST=Seoul/L=Uljiro/O=Hunature/OU=MakersAIOT/CN=hanwh-client"
 
 # Sign Client Certificate
 $OPENSSL_BIN ca -config $SCRIPT_DIR/ca.conf -days 365 -in client.csr -out client-cert.pem
